099ec00b2cbe8e70bafa3eee0b7c287b85e7f38f
selami538 <erdaltayfun37@gmail.com>
2026-05-27 02:56:47 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    
-    // GEÇİCİ TEST - direkt PHP'ye ilet
-    const phpRequest = new Request(
-      "http://87.236.19.13" + url.pathname + url.search,
-      {
-        method: request.method,
-        headers: {
-          ...Object.fromEntries(request.headers),
-          "Host": "altinoksoft.com"
-        },
-        body: ["GET", "HEAD"].includes(request.method) ? null : request.body
-      }
-    );
-    return fetch(phpRequest);
+    return new Response("Worker calisiyor!", {
+      headers: { "Content-Type": "text/plain" }
+    });
   }
 };
